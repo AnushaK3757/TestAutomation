9742c08eb99e2a2a7a6d4708d71e9e64b5b9da63
yyakeshwara <yakeshwararao.yellaboina2@t-mobile.com>
2021-02-04 19:02:54 -0800

    Test code

--- src/tests/pages/GoogleSearchPage.java
+++ src/tests/pages/GoogleSearchPage.java
@@ -1,9 +1,13 @@
 package pages;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.Reporter;
 
 public class GoogleSearchPage {
+
+    public WebDriver driver;
 
     @FindBy(xpath = "//input[@title='Search']")
     public WebElement textBox_GoogleSearch;
@@ -14,6 +18,12 @@ public class GoogleSearchPage {
     @FindBy(xpath = " //*[contains(text(),'Duck - Wikipedia')]")
     public WebElement text_GoogleResult;
 
+    public GoogleSearchPage(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+
+    }
+
     public boolean verifySearchForDucksOnGoogle(){
         boolean isExpectedverified = false;
         textBox_GoogleSearch.sendKeys("Ducks");
@@ -23,7 +33,6 @@ public class GoogleSearchPage {
             Reporter.log("Expected link is displayed on google results");
         }else
             Reporter.log("Expected link is not displayed on google results");
-
         return isExpectedverified;
     }
 
--- src/tests/tests/GoogleSearchTest.java
+++ src/tests/tests/GoogleSearchTest.java
@@ -3,30 +3,31 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.Reporter;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import pages.*;
 
 public class GoogleSearchTest {
+    private WebDriver driver;
 
     @BeforeTest
     public void openBrowser(){
-
-        WebDriver driver;
-        System.setProperty("webdriver.chrome.driver", "/lib/chromeDriver/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "/Users/yyakeshwara/Downloads/chromedriver 7");
         driver = new ChromeDriver();
         driver.get("https://google.com");
-
-
     }
 
     @Test
     public void verifyGoogleSearchByText(){
-        GoogleSearchPage googleSearchpage = new GoogleSearchPage();
+        GoogleSearchPage googleSearchpage = new GoogleSearchPage(driver);
         Assert.assertTrue(googleSearchpage.verifySearchForDucksOnGoogle(),"Ducks test search not displayed expected results on search results");
         Reporter.log("Verified search and results displayed as expected.");
     }
 
-
+    @AfterTest
+    public void closeBrowser(){
+        driver.quit();
+    }
 
 }
